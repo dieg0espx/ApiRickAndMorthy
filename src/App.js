e3375f72cd3c73448dcd97904adcfa522473e4e6
Diego Espinosa <55413117+dieg0espx@users.noreply.github.com>
2023-01-25 15:12:43 -0600

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch(`https://rickandmortyapi.com/api/episode`);
+    const data = await response.json();
+    setData(data.results);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data && data.map((d) => {
+        return(
+          <div>
+            <p> <b>ID:</b>{d.id}    <b>NAME:</b>{d.name}</p>
+          </div>
+        ) 
+      })}
     </div>
   );
 }
